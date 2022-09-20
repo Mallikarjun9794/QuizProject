58bafeb79390fb31af135717cee993ac66c23f3e
rahulzbaviskar <rahulzbaviskar@gmail.com>
2022-09-21 00:13:56 +0530

    Created Student Module

--- MiniProject_Quiz/src/com/asmr/quiz/main/QuizTest.java
+++ MiniProject_Quiz/src/com/asmr/quiz/main/QuizTest.java
@@ -1,10 +1,14 @@
 package com.asmr.quiz.main;
 
+import java.sql.SQLException;
+
 public class QuizTest {
 
-	public static void main(String[] args) {
+public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
 		
+		// Mini_Demo m = new Mini_Demo();
+		
+		// m.getConnection();
 
-	}
-
+}
 }
